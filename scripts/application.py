49aec0616dc017ab5e292af03f653b764d5d06fe
Jonas Brandhoff <Cidsbaum@gmail.com>
2021-05-13 13:34:48 +0200

    Updated window
    implemented correct writing and reading of speck files
    added a debug sine wave with fft
    using subplots now for spectrum and fft
    exporting of reference spectra and taking of reference has been implemented
    planning on implementing average of multiple spectra -> paul has no clue how complex that can be

--- scripts/application.py
+++ scripts/application.py
@@ -24,65 +24,86 @@ from itertools import count
 import scipy.fftpack as ff
 from scipy.signal import blackman
 
-def refspectrum():
-    spec = sm.from_first_available()
-    spec.integration_time_micros(10000)
-    wavelengths = spec.wavelengths()
-    intensity = spec.intensities()
-   
-    spec.close()
-    return (wavelengths, intensity)
 
 
 ref_wav, ref_int = [1,2,3,4,5],[1,2,3,4,5]
-current_wave, current_int = [1,2,3,4,5],[1,2,3,4,5]
+current_wave, current_int = [1,2,3,4.3,5],[1,2.2783,3,4,5]
 lowerBounds = 0
 upperBounds = 1000
 substract = True
 doFFT = True
 doWriteAlways = False
 
-def debug(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    
+integrationTime = 10000
+averageTotal = 1
+averageCount = 1
+doAverage = False
+
+#No unopened devices
+spec = ""#sm.from_first_available()
+
+liveFig, (axSpec, axFFT) = plt.subplots(2,1)
+
 
 
 """
 Hier sind debug sachen da ich kein sts spectrometer habe
 """
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-line, = ax.plot([], [], lw=2)
-ani = matanimation.FuncAnimation(plt.gcf(), debug, interval=10)
+
+def debug(i):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    line.set_data(x, y)
+    axFFT.cla()
+    if doFFT:
+        
+        k = x
+        w=blackman(len(k))
+        transformed = ff.fft(w*y)
+        x = ff.fftfreq(len(k),k[1]-k[0])
+        axFFT.plot(x,np.abs(transformed))
+        axFFT.set_xlim(0,4)
+
+
+
+line, = axSpec.plot([], [], lw=2)
+axSpec.set_xlim(0,2)
+axSpec.set_ylim(-2,2)
+ani = matanimation.FuncAnimation(liveFig, debug, interval=10)
 
 
 def animate(i):
+    global spec,integrationTime, current_wave, current_int,ref_wav, ref_int, substract,doFFT
+    global liveFig, axSpec, axFFT
+    
     plt.cla()
-
-    spec = sm.from_first_available()
-    spec.integration_time_micros(10000)
-    wavelengths = spec.wavelengths()
-    intensity = spec.intensities() - ref_int
+    spec.integration_time_micros(integrationTime)
+    current_wave = spec.wavelengths()
+    current_int = spec.intensities()
+    
+    if substract:
+        current_int = current_int - ref_int
+    
     wavelengths_cut = []
     intensity_cut = []
-    for idx, wv in enumerate(wavelengths):
+    for idx, wv in enumerate(current_wave):
         if wv > lowerBounds and wv < upperBounds:
-            wavelengths_cut.append(wavelengths[idx])
-            intensity_cut.append(intensity[idx])
+            wavelengths_cut.append(current_wave[idx])
+            intensity_cut.append(current_int[idx])
             
     wav = np.asarray(wavelengths_cut)
     its = np.asarray(intensity_cut)
 
-    k = 2*np.pi/wav
-    w=blackman(len(k))
-    transformed = ff.fft(w*its)
-    x = ff.fftfreq(len(k),k[1]-k[0])
-    #plt.plot(ref_wav,ref_int)
-    #plt.plot(k,its)
-    plt.plot(x,np.abs(transformed))
-    spec.close()
+    axSpec.plot(wav,its)
+
+
+    if doFFT:
+        k = 2*np.pi/wav
+        w=blackman(len(k))
+        transformed = ff.fft(w*its)
+        x = ff.fftfreq(len(k),k[1]-k[0])
+        axFFT.plot(x,np.abs(transformed))
+
 
 
 
@@ -93,6 +114,11 @@ class Window(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.connectSignalsSlots()
 
+
+    """
+    HIER WERDEN DIE EINZELNEN SIGNALS MIT DEN SLOTS VERBUNDEN
+    siehe QT5 Doc
+    """
     def connectSignalsSlots(self):
        self.btnLoadRef.clicked.connect(self.loadReference)
        self.btnStart.clicked.connect(self.startSampling)
@@ -105,15 +131,31 @@ class Window(QMainWindow, Ui_MainWindow):
        self.checkWrite.clicked.connect(self.updateParams)
        self.spinLower.valueChanged.connect(self.updateParams)
        self.spinUpper.valueChanged.connect(self.updateParams)
+       
+       self.spinAverage.valueChanged.connect(self.updateParams)
+       self.spinInt.valueChanged.connect(self.updateParams)
+       self.checkAverage.clicked.connect(self.updateParams)
 
        
+    """
+    oeffnet einen file dialog und schaut ob eine datei ausgewaelt wurde,
+    dann wird das spektrum als ref geoeffnet
+    """
     def loadReference(self):
+        global ref_int
+        global ref_wav
         file = QFileDialog.getOpenFileName()
-    
+        if file[0] != '':
+            data = pd.read_csv(file[0], sep=',', decimal='.')
+            ref_wav = data["wavelength"].values
+            ref_int = data["intensity"].values
+        
+       
+
 
     def startSampling(self):
         
-        #ani = matanimation.FuncAnimation(plt.gcf(), debug, interval=10)
+     
        
         ani.event_source.start()
         plt.show()
@@ -124,25 +166,58 @@ class Window(QMainWindow, Ui_MainWindow):
     def showRef(self):
         fig, ax = plt.subplots()
         fig.suptitle('Reference Spectra', fontsize=16)
-        ax.plot(ref_wav,ref_wav, label="Reference Spectra")
+        ax.plot(ref_wav,ref_int, label="Reference Spectra")
         ax.set_xlabel("Wavelength")
         ax.set_ylabel("Intensity")
         fig.show()
 
+
+    """
+    Speichert das momentane Spektrum ab
+    """
     def saveSpec(self):
         df = pd.DataFrame()
         df.insert(0,"wavelength",current_wave)
         df.insert(0,"intensity",current_int)
-        df.to_csv("spectrum_"+str(datetime.now())+".csv")
+        df.to_csv("spectrum_"+str(datetime.now())+".speck") #Diese endung beibehalten, ist eig nur eine csv aber muss
+                                                            #ja keiner wissen 
 
 
+    def takeRef(self):
+        global spec,integrationTime,ref_wav, ref_int, substract,doFFT
+        global liveFig, axSpec, axFFT
+        
+        spec.integration_time_micros(integrationTime)
+        ref_wav = spec.wavelengths()
+        ref_int = spec.intensities()
+        
+        df = pd.DataFrame()
+        df.insert(0,"wavelength",ref_wav)
+        df.insert(0,"intensity",ref_int)
+        df.to_csv("reference"+str(datetime.now())+".speck")
+        
+        
+        
+    """
+    Falls an den parameteren geschraubt wird soll das aktualisiert werden
+    """
     def updateParams(self):
+        
+        
+        global doFFT, doAlwaysWrite, substract, lowerBounds, upperBounds, doAverage, averageTotal, integrationTime
+        
+        
         doFFT = self.checkFFT.isChecked()
         doAlwaysWrite = self.checkWrite.isChecked()
         substract = self.checkSub.isChecked()
         
         lowerBounds = self.spinLower.value()
         upperBounds = self.spinUpper.value()
+        
+        doAverage = self.checkAverage.isChecked()
+        averageTotal = self.spinAverage.value()
+        
+        integrationTime = self.spinInt.value()
 
 
 
