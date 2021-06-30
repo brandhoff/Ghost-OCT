56a95443253ff593499a645c866adf618432e857
Jonas Brandhoff <Cidsbaum@gmail.com>
2021-06-30 18:01:05 +0200

    aktuelle scripts
    added transparent lines for peaks
    also made y axis same and seemless -> small border between?

--- auswertungs_scripts/Platte_gold.py
+++ auswertungs_scripts/Platte_gold.py
@@ -21,22 +21,22 @@ from matplotlib.colors import ListedColormap
 import matplotlib as mpl
 from collections import OrderedDict
 
-cmap = pl.cm.cool
 
-# Get the colormap colors
-my_cmap = cmap(np.arange(cmap.N))
+colors = ['cadetblue','yellowgreen'
+,
+'darkgoldenrod',
 
-# Set alpha
-my_cmap[:,-1] = np.linspace(0, 1, cmap.N)
+'coral']
 
-# Create new colormap
-my_cmap = ListedColormap(my_cmap)
-colors = cmap(np.linspace(0,1,6))
+
+annot_colors = ['red',
+'cornflowerblue',
+'mediumvioletred']
 
 datas = []
 
+filePRefix = r"/Users/jonas/OCT/scripts/gold_plaettchen/"
 
-filePRefix = r"D:\uni\PPraktikum\OCT\scripts\messwerte\gold_plaettchen//"
 maxima = []
 
 for i in range(1,7):
@@ -46,85 +46,123 @@ for i in range(1,7):
 
 
 
-print(datas)
-fig, axes = plt.subplots(3,2,sharey=True, facecolor='w',figsize=(11,8))
+fig, axes = plt.subplots(1,3,sharey=True,sharex=True, gridspec_kw={'wspace': 0,'hspace': 0}, facecolor='w',figsize=(11,8))
 
-zeile = 0
+
+
+
+drops = []
+
+for j, Xs in enumerate(datas[0]['x']):
+    if(Xs < 0):
+     drops.append(j)
+cuts = []
 for i,data in enumerate(datas):
-    
-    if(i % 2 == 0) :
-        data.plot.scatter(ax = axes[zeile][0], s=5, x='x', y= 'intensity',c = colors[i])
-    else:
-        data.plot.scatter(ax = axes[zeile][1], s=5, x='x', y= 'intensity',c = colors[i])
-        zeile += 1
+    cuts.append(data.drop(data.index[drops]))
+
+
+#Drei asugewaehlte plots um zu unbeschichtet besser zu vergleichen
+#eventuell beide uebereinander?
+cuts[1].plot.scatter(ax = axes[0], s=5, x='x', y= 'intensity',c = colors[0])
+cuts[2].plot.scatter(ax = axes[1], s=5, x='x', y= 'intensity',c = colors[1])
+cuts[3].plot.scatter(ax = axes[2], s=5, x='x', y= 'intensity',c = colors[2])
+
+
 fig.tight_layout()
 
 
-axes[0][0].axvline(-110, linewidth=2, color=colors[0])
-axes[0][1].axvline(-101, linewidth=2, color=colors[1])
-axes[1][0].axvline(-110, linewidth=2, color=colors[2])
-axes[1][1].axvline(-119.5, linewidth=2, color=colors[3])
-axes[2][0].axvline(-140, linewidth=2, color=colors[4])
-axes[2][1].axvline(-30, linewidth=2, color=colors[5])
-
-yUpperBound = 35
 
 
-for i  in range(0,3):
-    for j in range(0,2):
-        axes[i][j].set_ylim(0,yUpperBound)
-        axes[i][j].axvline(-40, linewidth=2, color='red')
-        axes[i][j].axvline(-12, linewidth=2, color='green')
 
-"""
-legend_elements = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
-                   Line2D([0], [0], color=colors[0], lw=2, label='$x= 21.5\mu m$'),
-                   Line2D([0], [0], color=colors[1], lw=2, label='$x= 106.5\mu m$'),
-                   Line2D([0], [0], color=colors[2], lw=2, label='$x= 127.5\mu m$'),
-                   Line2D([0], [0], color='red', lw=2, label='Constant Peak at: '),
-                   Line2D([0], [0], color='none', lw=2, label='$x= 152.0\mu m$')]
 
-axes[2].legend(handles=legend_elements,fontsize = 12,loc=(0.33,0.775)) 
+fig.tight_layout()
 
-yUpperBound = 200
+
+axes[1].tick_params(
+    axis='y',          
+    which='both',      
+    left=False,      
+    right=False,        
+    labelleft=False)
+axes[2].tick_params(
+    axis='y',          
+    which='both',      
+    left=False,      
+    right=False,        
+    labelleft=False)
+
+axes[0].spines['right'].set_visible(False)
+
+axes[1].spines['left'].set_visible(False)
+axes[1].spines['right'].set_visible(False)
+
+axes[2].spines['left'].set_visible(False)
+
+axes[0].tick_params(axis="y",direction="in", pad=-22)
+
+axes[0].tick_params(axis="x",direction="in")
+axes[1].tick_params(axis="x",direction="in")
+axes[2].tick_params(axis="x",direction="in")
+
+
+
+
+yUpperBound = 70
+
+
+
 axes[0].set_ylim(0,yUpperBound)
 axes[1].set_ylim(0,yUpperBound)
 axes[2].set_ylim(0,yUpperBound)
 
-bbox_props = dict(boxstyle="circle",pad=0.1,fc=(0.8,0.9,0.9,0),  ec="red", lw=1)
-#axes[0].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
-#axes[1].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
-#axes[2].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
 
-axes[0].axvline(-21.5, linewidth=2, color=colors[0])
-#axes[0].text(2.2,25000,'HOMO', color = 'black', rotation=90, fontsize=17)
-
-axes[1].axvline(-106.5, linewidth=2, color=colors[1])
-axes[1].axvline(-45.1, linewidth=2, color='cornflowerblue')
+axes[0].set_xlim(0,170)
+axes[1].set_xlim(0,170)
+axes[2].set_xlim(0,170)
 
 
-axes[2].axvline(-127.8, linewidth=2, color=colors[2])
-axes[2].axvline(-25.1, linewidth=2, color='cornflowerblue')
+
+linewidths = 14
 
 
-axes[0].axvline(-152, linewidth=2, color='red')
-axes[1].axvline(-152, linewidth=2, color='red')
-axes[2].axvline(-152, linewidth=2, color='red')
+axes[0].axvline(101, linewidth=linewidths, color=colors[0], alpha=0.1)
+axes[1].axvline(110, linewidth=linewidths, color=colors[1], alpha=0.1)
+axes[2].axvline(119.5, linewidth=linewidths, color=colors[2], alpha=0.1)
 
-axes[1].text(-43.1,120,'Peak at', color = 'cornflowerblue', fontsize=12)
-axes[1].text(-43.1,115,'$x= 45.0\mu m$', color = 'cornflowerblue', fontsize=12)
+axes[0].set_ylabel('Intensity in a.u.',fontsize=12)
 
-axes[2].text(-23.1,120,'Peak at', color = 'cornflowerblue', fontsize=12)
-axes[2].text(-23.1,115,'$x= 25.0\mu m$', color = 'cornflowerblue', fontsize=12)
-
-
+axes[0].tick_params(labelleft=False)
 axes[0].set_xlabel('')
 axes[1].set_xlabel('X relative to equal mirror distances in $\mu m$',fontsize=12)
 
 axes[-1].set_xlabel('')
 
-axes[0].set_ylabel('Intensity in a.u.',fontsize=12)
 
-axes[0].tick_params(labelleft=False)
-"""
+legend_ax0 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[0], lw=2, label='$x= 101\mu m$')]
+
+legend_ax1 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[1], lw=2, label='$x= 110\mu m$')]
+
+legend_ax2 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[2], lw=2, label='$x= 120\mu m$')]
+
+
+
+axes[0].legend(handles=legend_ax0,fontsize = 12,loc=(0.25,0.9)) 
+axes[1].legend(handles=legend_ax1,fontsize = 12,loc=(0.25,0.9)) 
+axes[2].legend(handles=legend_ax2,fontsize = 12,loc=(0.25,0.9)) 
+
+
+axes[0].axvline(163, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
+axes[1].axvline(163, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
+axes[2].axvline(163, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
+
+
+axes[0].axvline(12, linewidth=linewidths, color=annot_colors[1], alpha=0.1)
+axes[1].axvline(12, linewidth=linewidths, color=annot_colors[1], alpha=0.1)
+axes[2].axvline(12, linewidth=linewidths, color=annot_colors[1], alpha=0.1)
+
+axes[1].axvline(42, linewidth=linewidths, color=annot_colors[2], alpha=0.1)
+axes[2].axvline(42, linewidth=linewidths, color=annot_colors[2], alpha=0.1)
 plt.savefig("goldPlot.pdf");
--- auswertungs_scripts/Probe.py
+++ auswertungs_scripts/Probe.py
@@ -20,24 +20,43 @@ from matplotlib.colors import ListedColormap
 import matplotlib as mpl
 from collections import OrderedDict
 
-cmap = pl.cm.viridis
+"""
+---Maincolors---
+cadetblue
+coral
+darkgoldenrod
+yellowgreen
+---annotations---
+red
+cornflowerblue
+mediumvioletred
+"""
 
-# Get the colormap colors
-my_cmap = cmap(np.arange(cmap.N))
 
-# Set alpha
-my_cmap[:,-1] = np.linspace(0, 1, cmap.N)
 
-# Create new colormap
-my_cmap = ListedColormap(my_cmap)
-colors = cmap(np.linspace(0,1,4))
+colors = ['cadetblue','yellowgreen'
+,
+'darkgoldenrod',
+
+'coral']
+
+
+annot_colors = ['red',
+'cornflowerblue',
+'mediumvioletred']
 
 datas = []
 
-
-filePRefix = r"D:\uni\PPraktikum\OCT\scripts\messwerte\platte_unbeschichtet//"
+file2 = '/Users/jonas/OCT/scripts/gold_plaettchen/20nmgold_plaettchen_pos2_10ym.speck'
+filePRefix = r"/Users/jonas/OCT/scripts/platte_unbeschichtet/"
 maxima = []
+data_spek = filePRefix+ r'plaettchen_unbeschichtet_1.speck'
 
+#
+dataImp = pd.read_csv(data_spek, sep=',')
+dataImp.plot( x='wavelength', y= 'intensity',c = 'red')
+dataImp = pd.read_csv(file2, sep=',')
+dataImp.plot( x='wavelength', y= 'intensity',c = 'red')
 for i in range(1,4):
     data = pd.read_csv(filePRefix+"plaettchen_unbeschichtet_"+ str(i)+"_FFT.speck", sep=',')
     data['x'] = data['x'] / 1000
@@ -47,60 +66,128 @@ data_switch = datas[-1]
 datas[-1] = datas[1]
 datas[1] = data_switch
 
-print(datas)
-fig, axes = plt.subplots(1,3,sharey=True, facecolor='w',figsize=(11,8))
-for i,data in enumerate(datas):
-    data.plot.scatter(ax = axes[i], s=5, x='x', y= 'intensity',c = colors[i])
+fig, axes = plt.subplots(1,3,sharey=True, gridspec_kw={'wspace': 0}, facecolor='w',figsize=(11,8))
+
 fig.tight_layout()
 
-legend_elements = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
-                   Line2D([0], [0], color=colors[0], lw=2, label='$x= 21.5\mu m$'),
-                   Line2D([0], [0], color=colors[1], lw=2, label='$x= 106.5\mu m$'),
-                   Line2D([0], [0], color=colors[2], lw=2, label='$x= 127.5\mu m$'),
+
+axes[1].tick_params(
+    axis='y',          
+    which='both',      
+    left=False,      
+    right=False,        
+    labelleft=False)
+axes[2].tick_params(
+    axis='y',          
+    which='both',      
+    left=False,      
+    right=False,        
+    labelleft=False)
+
+axes[0].spines['right'].set_visible(False)
+
+axes[1].spines['left'].set_visible(False)
+axes[1].spines['right'].set_visible(False)
+
+axes[2].spines['left'].set_visible(False)
+
+axes[0].tick_params(axis="y",direction="in", pad=-22)
+
+axes[0].tick_params(axis="x",direction="in")
+axes[1].tick_params(axis="x",direction="in")
+axes[2].tick_params(axis="x",direction="in")
+
+
+legend_elements = [
+                   
                    Line2D([0], [0], color='red', lw=2, label='Constant Peak at: '),
                    Line2D([0], [0], color='none', lw=2, label='$x= 152.0\mu m$')]
 
-axes[2].legend(handles=legend_elements,fontsize = 12,loc=(0.33,0.775)) 
+
+
+legend_ax0 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[0], lw=2, label='$x= 21.5\mu m$')]
+
+legend_ax1 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[1], lw=2, label='$x= 106.5\mu m$')]
+
+legend_ax2 = [Line2D([0], [0], color='none', lw=2, label='Mirror at '),
+                   Line2D([0], [0], color=colors[2], lw=2, label='$x= 127.5\mu m$')]
+
+
+
+axes[0].legend(handles=legend_ax0,fontsize = 12,loc=(0.25,0.9)) 
+axes[1].legend(handles=legend_ax1,fontsize = 12,loc=(0.25,0.9)) 
+axes[2].legend(handles=legend_ax2,fontsize = 12,loc=(0.25,0.9)) 
+
+
+#axes[2].legend(handles=legend_elements,fontsize = 12,loc=(0.33,0.775)) 
 
 yUpperBound = 200
 axes[0].set_ylim(0,yUpperBound)
 axes[1].set_ylim(0,yUpperBound)
 axes[2].set_ylim(0,yUpperBound)
 
+
+axes[0].set_xlim(0,170)
+axes[1].set_xlim(0,170)
+axes[2].set_xlim(0,170)
+
 bbox_props = dict(boxstyle="circle",pad=0.1,fc=(0.8,0.9,0.9,0),  ec="red", lw=1)
 #axes[0].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
 #axes[1].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
 #axes[2].annotate("   ",(15,27),fontsize=18, color = "red", xycoords="axes points", bbox=bbox_props)
 
-axes[0].axvline(-21.5, linewidth=2, color=colors[0])
-#axes[0].text(2.2,25000,'HOMO', color = 'black', rotation=90, fontsize=17)
-
-axes[1].axvline(-106.5, linewidth=2, color=colors[1])
-axes[1].axvline(-45.1, linewidth=2, color='cornflowerblue')
 
 
-axes[2].axvline(-127.8, linewidth=2, color=colors[2])
-axes[2].axvline(-25.1, linewidth=2, color='cornflowerblue')
+linewidths = 14
+
+axes[0].axvline(21.5, linewidth=linewidths, color=colors[0], alpha=0.1)
+
+axes[1].axvline(106.5, linewidth=linewidths, color=colors[1], alpha=0.1)
+axes[1].axvline(45.1, linewidth=linewidths, color=annot_colors[1], alpha=0.1)
 
 
-axes[0].axvline(-152, linewidth=2, color='red')
-axes[1].axvline(-152, linewidth=2, color='red')
-axes[2].axvline(-152, linewidth=2, color='red')
-
-axes[1].text(-43.1,120,'Peak at', color = 'cornflowerblue', fontsize=12)
-axes[1].text(-43.1,115,'$x= 45.0\mu m$', color = 'cornflowerblue', fontsize=12)
-
-axes[2].text(-23.1,120,'Peak at', color = 'cornflowerblue', fontsize=12)
-axes[2].text(-23.1,115,'$x= 25.0\mu m$', color = 'cornflowerblue', fontsize=12)
+axes[2].axvline(127.8, linewidth=linewidths, color=colors[2], alpha=0.1)
+axes[2].axvline(25.1, linewidth=linewidths, color=annot_colors[1], alpha=0.1)
 
 
-axes[0].set_xlabel('')
-axes[1].set_xlabel('X relative to equal mirror distances in $\mu m$',fontsize=12)
+axes[0].axvline(152, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
+axes[1].axvline(152, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
+axes[2].axvline(152, linewidth=linewidths, color=annot_colors[0], alpha=0.1)
 
-axes[-1].set_xlabel('')
+
+axes[1].text(43.1,120,'Peak at', color = annot_colors[1], fontsize=12)
+axes[1].text(43.1,115,'$x= 45.0\mu m$', color = annot_colors[1], fontsize=12)
+
+axes[2].text(23.1,120,'Peak at', color = annot_colors[1], fontsize=12)
+axes[2].text(23.1,115,'$x= 25.0\mu m$', color = annot_colors[1], fontsize=12)
+
+
+
+
+
+
+
+drops = []
+
+for j, Xs in enumerate(datas[0]['x']):
+    if(Xs < 0):
+     drops.append(j)
+cuts = []
+for i,data in enumerate(datas):
+    cuts.append(data.drop(data.index[drops]))
+
+for i,data in enumerate(cuts):
+    
+    data.plot.scatter(ax = axes[i], s=5, x='x', y= 'intensity',c = colors[i])
+
 
 axes[0].set_ylabel('Intensity in a.u.',fontsize=12)
 
 axes[0].tick_params(labelleft=False)
+axes[0].set_xlabel('')
+axes[1].set_xlabel('X relative to equal mirror distances in $\mu m$',fontsize=12)
 
+axes[-1].set_xlabel('')
 plt.savefig("unbeschichetPlot.pdf");
